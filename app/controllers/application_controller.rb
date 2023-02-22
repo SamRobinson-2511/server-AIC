3064b7f7d0d864ac7babab3b3396d130351a6f6d
Sam Robinson <110217039+SamRobinson-2511@users.noreply.github.com>
2023-02-22 13:11:15 -0600

    controller actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,20 @@
 class ApplicationController < ActionController::API
-    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
     
-    
-    
-    
-    private
-    def record_not_found
-        render json: {errors: ['Record not found']}, status: :not_found
+
+    def current_viewer
+        auth_token = request.headers['auth-token']
+        if auth_token
+            token = JWT.decode( auth_token, ENV['JWT_TOKEN'])[0]
+            return Viewer.find_by( id: token['viewer'])
+        else
+            return nil
+            render json: { message: ['No token found']}, status: :unprocessable_entity
+        end
+    end
+
+    def authorize!
+        unless current_viewer 
+            render json: { errors: ['You must be logged in to do that']}, status: :unauthorized
+        end
     end
 end
